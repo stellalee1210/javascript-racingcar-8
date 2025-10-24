f6bfb4be7800369e66b3f3c39507fa9cea48bc94
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-24 15:11:36 +0900

    feat(validator): 검증 로직 추가
    -입력된 자동차 이름 글자 수 검증 로직 추가
    -입력된 게임 진행 횟수 숫자 판별 검증 로직 추가

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -3,4 +3,20 @@ export class Validator {
     if (input.length === 0) throw Error("[ERROR] 칸이 비어있습니다.");
     return false;
   }
+
+  isCarNameUnderLimit(carName) {
+    if (carName.length > 5)
+      throw Error(
+        `[ERROR] 자동차 이름 '${carName}'이(가) 제한 글자 수를 넘어섰습니다. (제한 글자 수 : 5글자)`
+      );
+    return true;
+  }
+
+  isCountNumber(count) {
+    if (isNaN(Number(count)))
+      throw Error(
+        "[ERROR] 입력하신 게임 진행 횟수는 숫자가 아닙니다. 숫자로 다시 입력해주세요."
+      );
+    return true;
+  }
 }
