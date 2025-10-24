e71f82cb3020d2ebb8cf68280119bd57f66527e6
stellalee1210 <yeobinlee1210@gmail.com>
2025-10-24 16:05:10 +0900

    fix(parser): 자동차 이름 파싱 시 앞뒤 공백 제거 기능 추가

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,7 +1,7 @@
 export const parseInputToArray = (rawCarNameInput) => {
   try {
     const carNames = rawCarNameInput.split(",");
-    return carNames;
+    return carNames.map((v) => v.trim());
   } catch (error) {
     throw Error("[ERROR] 자동차 이름 구분자가 잘못 들어가있습니다.");
   }
